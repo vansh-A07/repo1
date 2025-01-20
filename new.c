fe007d8f18a26fd9fc33742d3cdde81267f835f8
Manas171106 <manasgarg924@gmail.com>
2025-01-20 14:38:34 +0530

    Add files via upload

---
+++ new.c
@@ -0,0 +1,182 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+// void function(){
+//     int x = 10;
+//     printf("%d",x);
+// }
+
+// int main(){function();}
+
+//
+// void function()
+// {
+//     int x = 20;
+//     static int y = 30;
+//     x = x + 10;
+//     y = y + 10;
+//     printf("\tLocal: %d\n\tStatic: %d\n", x, y);
+// }
+// int main()
+// {
+//     printf("First call\n");
+//     function();
+//     printf("Second call\n");
+//     function();
+//     return 0;
+// }
+
+
+
+
+
+
+// int main() {
+// int a = 9;
+// int b = -9;
+// int c = 89U;
+// long int d = 99998L;
+// printf("Integer value with positive data: %d\n",a);
+// printf("Integer value with negative data: %d\n",b);
+// printf("Integer value with an unsigned int data: %d\n",c);
+
+// printf("Integer value with an long int data : %d",d);
+// }
+
+
+
+
+
+
+// int main() {
+// char a = 'a';
+// char c;
+// printf("Value of a : %c\n ",a);
+// a++;
+// printf("Value of a after increment: %c\n" ,a);
+
+// c = 99;
+// printf("Value of c : %c\n",c);
+// return 0;
+// }
+
+
+
+
+
+// int main() {
+// float a = 9.0f;
+// float b = 2.5f;
+
+// float c = 2E-4f;
+// printf("%f\n",a);
+// printf("%f\n",b);
+// printf("%f\n",c);
+
+// return 0;
+// }
+
+
+
+
+
+
+// int main() {
+// double a = 123123123.00;
+// double b = 12.293123;
+// double c = 2312312312.123123;
+// printf("%lf\n",a);
+// printf("%lf\n",b);
+// printf("%lf\n",c);
+// return 0;
+// }
+
+
+
+
+
+
+// int main() {
+// int val = 30;
+// void* ptr = &val;
+// printf("%d",*(int*)ptr);
+// return 0;
+// }
+
+
+
+
+
+
+// int main() {
+// int a = 10, b = 4,res;
+// printf("a is the %d and b is %d\n",a,b);
+// res = a + b;
+// printf("a + b is %d\n",res);
+// res = a - b;
+// printf("a - b is %d\n",res);
+// res = a * b;
+// printf("a * b is %d\n",res);
+// res = a / b;
+// printf("a / b is %d\n",res);
+// res = a%b;
+// printf("a % b is %d\n",res);
+// return 0;
+    
+// }
+
+
+
+
+
+
+
+// int main() {
+// int a = 10, b = 20;
+// if (a > 0 && b > 0){
+//     printf("both values are greater than 0\n");
+// }
+// else{
+//     printf("both values are less than 0");
+// }
+// return 0;
+// }
+
+
+
+
+
+
+// int main() {
+// unsigned int a = 5,b = 9;
+// printf("a = %u,b = %u\n",a,b);
+// printf("a&b = %u\n",a&b);
+// printf("a|b = %u\n",a|b);
+// printf("a ^ b = %u\n",a^b);
+// printf("~a = %U\n",~a);
+// printf("b<<1 = %u\n",b<<1);
+// printf("b>>1 = %u",b>>1);
+
+// return 0;
+// }
+
+
+
+
+
+
+
+// int main() {
+// int a = 10;
+// printf("Value of a is %d\n",a);
+// a += 10;
+// printf("value of a is %d\n",a);
+// a -= 10;
+// printf("value of a is %d\n",a );
+// a *=10;
+// printf("value of a is %d\n",a);
+// a /= 10;
+// printf("value of a is %d\n",a);
+
+// return 0;
+// }
